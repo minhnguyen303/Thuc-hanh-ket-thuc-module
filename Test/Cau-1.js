44d05d1e6a76ff9c8aecf1413ec2ed94c1ec3ea4
minhnguyen303 <minh09az09@gmail.com>
2020-12-03 11:14:39 +0700

    change somethings

--- Test/Cau-1.js
+++
@@ -1,8 +0,0 @@
-function findMax(arr){
-    let max = 0;
-    for (let num of arr){
-        if (max < num)
-            max = num;
-    }
-    return max;
-}
--- Test/Cau-2.js
+++
@@ -1,7 +0,0 @@
-function tryRemoveFromArray(arr, x){
-    for (let i in arr){
-        if (arr[i] == x)
-            arr.splice(i, 1);
-    }
-    return arr;
-}
